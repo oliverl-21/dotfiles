ade2c108f5ffc4e7b9eadf91a09e9be1d5bdd44f
oliverl-21 <77393143+oliverl-21@users.noreply.github.com>
2023-09-26 15:13:17 +0200

    relativenumber enabled

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -2,6 +2,7 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+vim.wo.relativenumber = true
 return {
-    vim.keymap.set('n', '<C-b>', '<Cmd>Neotree toggle<CR>')
+    vim.keymap.set('n', '<C-b>', '<Cmd>Neotree toggle<CR>'),
 }
