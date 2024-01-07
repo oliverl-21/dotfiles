76addc7a04560ba8d1181ffff6980115ab4fca35
oliverl-21 <77393143+oliverl-21@users.noreply.github.com>
2024-01-07 10:27:15 +0100

    mason, autopairs, lspconfig

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,6 +1,7 @@
 return {
   'neovim/nvim-lspconfig',
   event = { 'BufReadPre', 'BufNewFile' },
+  lazy = true,
   dependencies = {
     'hrsh7th/cmp-nvim-lsp',
     { 'antosha417/nvim-lsp-file-operations', config = true },
